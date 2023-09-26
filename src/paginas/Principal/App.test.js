aa4f42778308b7257b76746922400686bf5777d4
Hosana <hosanabarceloscandido@gmail.com>
2023-09-26 15:48:40 -0300

    test: add caso de teste para navegação entre páginas após clique

--- src/paginas/Principal/App.test.js
+++ src/paginas/Principal/App.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import AppRoutes from '../../routes';
 
 describe('Main component', () => {
     test('If the transaction is added to the statement', () => {
@@ -19,5 +20,17 @@ describe('Main component', () => {
         const extractItem = screen.getByRole('listitem');
 
         expect(newTransition).toContainElement(extractItem);
-    })
+    });
+
+    test('If navigation between pages occurs after clicking', async () => {
+        render(<AppRoutes />, {wrapper: BrowserRouter});
+
+        const cardLinkPage = screen.getByText('Cartões');
+        expect(cardLinkPage).toBeInTheDocument();
+
+        userEvent.click(cardLinkPage);
+
+        const title = await screen.findByText('Meus cartões');
+        expect(title).toBeInTheDocument();
+    });
 });
