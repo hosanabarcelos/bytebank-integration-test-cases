0d0590fae2fc4f3521399e88c16060dbf19ed549
Hosana <hosanabarceloscandido@gmail.com>
2023-09-26 11:28:02 -0300

    test: add caso de teste para renderizar a rota de cartões

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,11 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
 import App from './paginas/Principal/App';
+import Cartoes from './componentes/Cartoes'
 
 describe('Routes', () => {
   test('If the main route is rendered correctly', () => {
     render(<App />, { wrapper: BrowserRouter });
     const user = screen.getByText('Olá, Joana :)!');
     expect(user).toBeInTheDocument();
+  });
+
+  test('If the cards route was rendered', () => {
+    const route = '/cartoes';
+
+    render(
+        <MemoryRouter initialEntries={[route]}>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="cartoes" element={<Cartoes />}/>
+                </Route>
+            </Routes>
+        </MemoryRouter>
+    );
+
+    const mycards = screen.getByText('Meus cartões');
+    expect(mycards).toHaveTextContent('Meus cartões');
   });
 });
