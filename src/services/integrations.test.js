6829045ab1f6ed5a1ca6b339f9d2b2727f7d684f
Hosana <hosanabarceloscandido@gmail.com>
2023-09-26 17:24:52 -0300

    test: add caso de teste com mock de chamada a api

--- src/services/integrations.test.js
+++ src/services/integrations.test.js
@@ -1,15 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import api from './api';
 import { buscaTransacoes } from './transacoes';
-import App from '../paginas/Principal/App';
 
-describe('API requests', () => {
+jest.mock('./api');
+
+const mockTransaction = [
+    {
+        id: 1,
+        transacao: 'Depósito',
+        valor: '100',
+        data: '25/09/2023',
+        mes: 'Novembro',
+    }
+];
+
+const mockRequest = (retorn) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          data: retorn,
+        });
+      }, 200);
+    });
+};
+
+describe('API Request', () => {
     test('If the list of transactions was returned', async () => {
-        const transactions = await buscaTransacoes();
-        expect(transactions).toHaveLength(3);
+      api.get.mockImplementation(() => mockRequest(mockTransaction));
 
-        render(<App />, { wrapper: BrowserRouter })
-        const transaction = await screen.findAllByText('Novembro');
-        transaction.forEach(transaction => expect(transaction).toBeInTheDocument());
+      const transacoes = await buscaTransacoes();
+
+      expect(transacoes).toEqual(mockTransaction);
+      expect(api.get).toHaveBeenCalledWith('/transacoes');
     });
 });
